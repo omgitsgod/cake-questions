26e0a21a52777021eeff9a155751e815da6ebe80
omgitsgod <omgitsgod@gmail.com>
2020-05-05 23:22:40 -0400

    Add canTwoMoviesFillFlight

---
+++ canTwoMoviesFillFlight.js
@@ -0,0 +1,18 @@
+function canTwoMoviesFillFlight(movieLengths, flightLength) {
+
+
+    let movies = new Set();
+
+    for (let i = 0; i < movieLengths.length; i++) {
+        let first = movieLengths[i];
+        let needTime = flightLength - first;
+
+        if (movies.has(needTime)) {
+            return true;
+        }
+
+        movies.add(first);
+    }
+
+    return false;
+}
