64a9486535f3fb34f975afe70b4c1dc34e3c0e04
omgitsgod <omgitsgod@gmail.com>
2020-05-05 23:29:39 -0400

    Fix formatting

--- mergeRanges.js
+++ mergeRanges.js
@@ -1,22 +1,20 @@
 function mergeRanges(meetings) {
 
-    let arr = []
-    let sorted = JSON.parse(JSON.stringify(meetings)).sort((a, b) => a.startTime - b.startTime)
-
+    let arr = [];
+    let sorted = JSON.parse(JSON.stringify(meetings)).sort((a, b) => a.startTime - b.startTime);
 
     for (let i = 0; i < sorted.length; i++) {
-        let meeting = sorted[i]
-        let last = arr[arr.length - 1] || { endTime: 0 }
+        let meeting = sorted[i];
+        let last = arr[arr.length - 1] || { endTime: 0 };
         if (last.endTime > meeting.startTime) { continue }
         for (let j = i + 1; j < sorted.length; j++) {
-            let next = sorted[j]
+            let next = sorted[j];
             if (meeting.endTime >= next.startTime && meeting.endTime < next.endTime) {
-                meeting.endTime = next.endTime
-                endTime = next.endTime
+                meeting.endTime = next.endTime;
+                endTime = next.endTime;
             }
         }
-        arr.push(meeting)
+        arr.push(meeting);
     }
     return arr;
 }
-
