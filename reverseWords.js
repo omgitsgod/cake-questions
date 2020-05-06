59eac70704f4a0d1038cff77533a6783edeee3dd
omgitsgod <omgitsgod@gmail.com>
2020-05-05 23:29:56 -0400

    Fix formatting

--- reverseWords.js
+++ reverseWords.js
@@ -1,25 +1,25 @@
 function reverseWords(message) {
 
-    let start = 0
-    reverseArray(message, start, message.length - 1)
+    let start = 0;
+    reverseArray(message, start, message.length - 1);
 
     for (let i = 0; i <= message.length; i++) {
         if (i === message.length || message[i] === ' ') {
-            reverseArray(message, start, i - 1)
-            start = i + 1
+            reverseArray(message, start, i - 1);
+            start = i + 1;
         }
     }
 }
 
 function reverseArray(arr, start, end) {
 
-    let mid = Math.floor((start + end) / 2)
-    let tail = end
-    let temp
+    let mid = Math.floor((start + end) / 2);
+    let tail = end;
+    let temp;
     for (let i = start; i <= mid; i++) {
-        temp = arr[i]
-        arr[i] = arr[tail]
-        arr[tail] = temp
-        tail--
+        temp = arr[i];
+        arr[i] = arr[tail];
+        arr[tail] = temp;
+        tail--;
     }
 }
