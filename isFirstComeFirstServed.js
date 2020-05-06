0d7a7bf14cd2a4302cc686c4a718a14f03965f3b
omgitsgod <omgitsgod@gmail.com>
2020-05-05 23:29:17 -0400

    Fix formatting

--- isFirstComeFirstServed.js
+++ isFirstComeFirstServed.js
@@ -1,23 +1,23 @@
 function isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders) {
 
-    let takeOut = 0
-    let dineIn = 0
-    let served = 0
+    let takeOut = 0;
+    let dineIn = 0;
+    let served = 0;
 
     if (servedOrders.length !== takeOutOrders.length + dineInOrders.length) {
-        return false
+        return false;
     }
 
     while (served < takeOutOrders.length + dineInOrders.length) {
         if (servedOrders[served] === takeOutOrders[takeOut]) {
-            ++takeOut
-            ++served
+            ++takeOut;
+            ++served;
         } else if (servedOrders[served] === dineInOrders[dineIn]) {
-            ++dineIn
-            ++served
-        } else return false
+            ++dineIn;
+            ++served;
+        } else return false;
     }
 
     if (served === takeOutOrders.length + dineInOrders.length) return true
-    else return false
+    else return false;
 }
