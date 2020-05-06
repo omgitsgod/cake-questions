f098228cf0e72e8fd91186fc1960e4be09d54333
omgitsgod <omgitsgod@gmail.com>
2020-05-05 23:29:46 -0400

    Fix formatting

--- reverse.js
+++ reverse.js
@@ -1,11 +1,10 @@
 function reverse(arrayOfChars) {
 
     let mid = Math.floor(arrayOfChars.length / 2)
-    let temp
+    let temp;
     for (let i = 0; i < mid; i++) {
-        temp = arrayOfChars[i]
-        arrayOfChars[i] = arrayOfChars[arrayOfChars.length - 1 - i]
-        arrayOfChars[arrayOfChars.length - 1 - i] = temp
+        temp = arrayOfChars[i];
+        arrayOfChars[i] = arrayOfChars[arrayOfChars.length - 1 - i];
+        arrayOfChars[arrayOfChars.length - 1 - i] = temp;
     }
 }
-
